b7ee126556a251df39f40def12afd4d098167aa7
Jeelpatel2003 <pateljeel3007@gmail.com>
2025-07-02 14:03:34 +0530

    h

---
+++ Controllers/AdminController .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WasteFood.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
