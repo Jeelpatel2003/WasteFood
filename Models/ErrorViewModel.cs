554c2a7cf7bda8f5ef03716fa41bd3d7c2fee2d6
Jeelpatel2003 <pateljeel3007@gmail.com>
2025-07-02 09:57:07 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WasteFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
